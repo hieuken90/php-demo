0f2669c7100f148aed1626281a429e45254c3915
hieuken90 <hieu90.gl@gmail.com>
2020-11-06 23:42:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-$myObj->name = "John";
-$myObj->age = 50;
-$myObj->city = "New York";
+$myObj->name = "{
+ "bListds":["mqqapi","mqqwpa","mqq","wechat","weixin","alipay","alipays","alipayshare","mqppwallet"],
+ "bSastds":"1",
+ "bTbdmao":"1",
+ "bJtgdex":"1",
+ "bUgqndm":"https://www.baidu.com/",
+ "bSaxrbt":"1",
+ "bTalistp":["jPFDA"],
+    "bIgsstd":"http://s8.mogucdn.com/p2/170223/28n_4eb3la6b6b0h78c23d2kf65dj1a92_750x1334.jpg",
+    "bInkafs":"http://wap.baidu.com",
+    "bQpenys":"0"
+}";
+// $myObj->age = 50;
+// $myObj->city = "New York";
+
 
 $myJSON = json_encode($myObj);
 
