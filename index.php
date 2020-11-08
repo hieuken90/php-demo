a3dee20600040b35bac77173c34e159e4d501412
hieuken90 <hieu90.gl@gmail.com>
2020-11-08 19:38:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 
 <?php
-$myObj->bListds = array("mqqapi","mqqwpa","mqq","wechat","weixin","alipay","alipays","alipayshare","mqppwallet");
-$myObj->bSastds = "1";
-$myObj->bTbdmao = "1";
-$myObj->bJtgdex = "1";
-$myObj->bUgqndm = "https://www.baidu.com/";
-$myObj->bSaxrbt = "1";
+$myObj->stringAds = "https://www.baidu.com/";
 
 $myJSON = json_encode($myObj);
 
